37c5e9702146c83b52e1ab80afd7ad65d94332a3
Ana Liebert <analiebert19@gmail.com>
2022-02-13 23:47:04 -0500

    initial css and model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+import time
 
 # Create your models here.
+
+# class Profile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     name = models.CharField(max_length=100)
+#     city = models.CharField(max_length=60)
+#     join_date = models.DateTimeField(auto_now_add=True)
