d709acb68d9442019cb8200c5ffa336aa4ef42fc
Ana Liebert <analiebert19@gmail.com>
2022-02-14 10:23:43 -0500

    model/css

--- main_app/models.py
+++ main_app/models.py
@@ -4,8 +4,8 @@ import time
 
 # Create your models here.
 
-# class Profile(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     name = models.CharField(max_length=100)
-#     city = models.CharField(max_length=60)
-#     join_date = models.DateTimeField(auto_now_add=True)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    city = models.CharField(max_length=60)
+    join_date = models.DateTimeField(auto_now_add=True)
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Profile
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.base import TemplateView
 
 # Create your views here.
+
+class Profile(TemplateView):
+    template_name = "profile.html"
+    def get_context_data(self, **kwargs): #needs to be written as get_context_data
+        context = super().get_context_data(**kwargs)
+        name = self.request.GET.get("name")
+        if name != None:
+            context["profile"] = Profile.objects.filter(
+                name__icontains=name, user=self.request.user)
+        else:
+            context["profile"] = Profile.objects.filter(user=self.request.user)
+        return context
