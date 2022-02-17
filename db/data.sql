a728a01fc4dc7d516d757493879632da59914e12
harrisongers <hjgerstenlauer@gmail.com>
2022-02-17 16:02:32 -0500

    reroute guide link on profile page

--- db/data.sql
+++ db/data.sql
@@ -1,75 +1,59 @@
--- INSERT INTO main_app_city(name, image, country, grid_img) 
--- VALUES (
--- 'Berlin',
--- 'https://www.worldatlas.com/r/w1200/upload/7b/92/51/shutterstock-1011630226.jpg',
--- 'Germany',
--- 'https://previews.123rf.com/images/franzidraws/franzidraws1604/franzidraws160400050/55798423-seamless-banner-of-berlin-s-skyline-hand-drawn-black-and-white-illustration.jpg'
--- );
+INSERT INTO main_app_guide(title, city_id, country, neighborhood, description, category, image, created_at, user_id)
+  VALUES
+    (
+    'Aufschnitt',
+    '1',
+    'Germany',
+    'Friedrichshain',
+    'Aufschnitt Berlin is one of the most unusual labels for the design of lifestyle products, textiles, and home accessories. The distinctive naive style of food realization in textile form, the love of materials and the knowledge of design manufacturing for over 12 years all contribute to make their products unique',
+    'Shops',
+    'https://onthegrid.city/imager/s3_amazonaws_com/onthegrid.city/assets/grid/berlin/friedrichshain/aufschnitt/OTG-Aufschnitt-2_299006722e285f47655d17d1c9136337.jpg',
+    '2021-05-07 18:57:42.517205-07',
+    '1'
+    );
 
--- INSERT INTO main_app_city(name, image, country, grid_img) 
--- VALUES (
--- 'Havana',
--- 'https://media.gq.com/photos/5914dd2943572d096b80c8f3/4:3/w_1728,h_1296,c_limit/guide-to-old-havana-cuba.jpg',
--- 'Cuba',
--- 'https://previews.123rf.com/images/franzidraws/franzidraws1606/franzidraws160600007/60559448-la-havane-cuba-dessin%C3%A9s-%C3%A0-la-main-illustration-en-noir-et-blanc.jpg'
--- );
-
--- INSERT INTO main_app_guide(title, city_id, country, neighborhood, description, category, image, created_at, user_id)
---   VALUES
---     (
---     'Aufschnitt',
---     '1',
---     'Germany',
---     'Friedrichshain',
---     'Aufschnitt Berlin is one of the most unusual labels for the design of lifestyle products, textiles, and home accessories. The distinctive naive style of food realization in textile form, the love of materials and the knowledge of design manufacturing for over 12 years all contribute to make their products unique',
---     'Shops',
---     'https://onthegrid.city/imager/s3_amazonaws_com/onthegrid.city/assets/grid/berlin/friedrichshain/aufschnitt/OTG-Aufschnitt-2_299006722e285f47655d17d1c9136337.jpg',
---     '2021-05-07 18:57:42.517205-07',
---     '1'
---     );
-
--- INSERT INTO main_app_guide(title, city_id, country, neighborhood, description, category, image, created_at, user_id)
---   VALUES
---     (
---     'St. Mauli',
---     '1',
---     'Germany',
---     'Friedrichshain',
---     'Maultaschen are sort of filled dumplings, coming from Bavaria. St. Mauli is bringing them to Berlin! This place is very cosy and the Maultaschen are very tasty. You can choose between veggie ones and meat ones, just as you like it!',
---     'Food',
---     'https://onthegrid.city/imager/s3_amazonaws_com/onthegrid.city/assets/grid/berlin/friedrichshain/st-mauli/stmauli-3_299006722e285f47655d17d1c9136337.jpg',
---     '2021-05-07 18:57:42.517205-07',
---     '1'
---     );
+INSERT INTO main_app_guide(title, city_id, country, neighborhood, description, category, image, created_at, user_id)
+  VALUES
+    (
+    'St. Mauli',
+    '1',
+    'Germany',
+    'Friedrichshain',
+    'Maultaschen are sort of filled dumplings, coming from Bavaria. St. Mauli is bringing them to Berlin! This place is very cosy and the Maultaschen are very tasty. You can choose between veggie ones and meat ones, just as you like it!',
+    'Food',
+    'https://onthegrid.city/imager/s3_amazonaws_com/onthegrid.city/assets/grid/berlin/friedrichshain/st-mauli/stmauli-3_299006722e285f47655d17d1c9136337.jpg',
+    '2021-05-07 18:57:42.517205-07',
+    '1'
+    );
 
 
--- INSERT INTO main_app_guide(title, city_id, country, neighborhood, description, category, image, created_at, user_id)
---   VALUES
---     (
---     'Fabrica de Arte Cubano',
---     '2',
---     'Cuba',
---     'Vedado',
---     'La Fabrica de Arte Cubano (FAC), is Havana’s hippest spot. A place unimaginable just a few years ago. It’s an art gallery, music venue, bar, cultural center, dance club all in one. It almost feels like you could be in Brooklyn (for better or worse). The crowd is a fun mix of Cubans and foreigners due to the low $2 cover, and reasonable drink prices',
---     'Arts',
---     'https://onthegrid.city/imager/s3_amazonaws_com/onthegrid.city/assets/grid/havana/vedado/fabrica-de-arte-cubano-fac/DSC4826_299006722e285f47655d17d1c9136337.jpg',
---     '2021-05-07 18:57:42.517205-07',
---     '1'
---     );
+INSERT INTO main_app_guide(title, city_id, country, neighborhood, description, category, image, created_at, user_id)
+  VALUES
+    (
+    'Fabrica de Arte Cubano',
+    '2',
+    'Cuba',
+    'Vedado',
+    'La Fabrica de Arte Cubano (FAC), is Havana’s hippest spot. A place unimaginable just a few years ago. It’s an art gallery, music venue, bar, cultural center, dance club all in one. It almost feels like you could be in Brooklyn (for better or worse). The crowd is a fun mix of Cubans and foreigners due to the low $2 cover, and reasonable drink prices',
+    'Arts',
+    'https://onthegrid.city/imager/s3_amazonaws_com/onthegrid.city/assets/grid/havana/vedado/fabrica-de-arte-cubano-fac/DSC4826_299006722e285f47655d17d1c9136337.jpg',
+    '2021-05-07 18:57:42.517205-07',
+    '1'
+    );
 
--- INSERT INTO main_app_guide(title, city_id, country, neighborhood, description, category, image, created_at, user_id)
---   VALUES
---     (
---     'Mercado Agropecuario 19 y B (Farmers Market)',
---     '2',
---     'Cuba',
---     'Vedado',
---     'Locals call this the diplomats farm stand for the higher-quality produce and elevated prices. Indeed, it is Havana’s version of whole foods, yet still a far far cry. But its a nice place to grab a few fruits and veggies, and to see how locals shop. You’ll find more things in the AM than the PM. Depending on seasons, you will find rare items like mint and other herbs, ripe tomatoes, and kiwis. Make sure to bargain, or they will take advantage of you.',
---     'Shops',
---     'https://onthegrid.city/imager/s3_amazonaws_com/onthegrid.city/assets/grid/havana/vedado/agropecuario-19-b/DSC5484_299006722e285f47655d17d1c9136337.jpg',
---     '2021-05-07 18:57:42.517205-07',
---     '1'
---     );
+INSERT INTO main_app_guide(title, city_id, country, neighborhood, description, category, image, created_at, user_id)
+  VALUES
+    (
+    'Mercado Agropecuario 19 y B (Farmers Market)',
+    '2',
+    'Cuba',
+    'Vedado',
+    'Locals call this the diplomats farm stand for the higher-quality produce and elevated prices. Indeed, it is Havana’s version of whole foods, yet still a far far cry. But its a nice place to grab a few fruits and veggies, and to see how locals shop. You’ll find more things in the AM than the PM. Depending on seasons, you will find rare items like mint and other herbs, ripe tomatoes, and kiwis. Make sure to bargain, or they will take advantage of you.',
+    'Shops',
+    'https://onthegrid.city/imager/s3_amazonaws_com/onthegrid.city/assets/grid/havana/vedado/agropecuario-19-b/DSC5484_299006722e285f47655d17d1c9136337.jpg',
+    '2021-05-07 18:57:42.517205-07',
+    '1'
+    );
 
 
 -- INSERT INTO main_app_city(name, image, country, grid_img) 
@@ -149,10 +133,26 @@
 
 
 
-INSERT INTO main_app_city(name, image, country, grid_img) 
-VALUES (
-'Barcelona',
-'https://cdn.britannica.com/26/116526-050-76C37BBC/Cathedral-of-St-Basil-the-Blessed-Moscow.jpg',
-'Spain',
-'https://previews.123rf.com/images/franzidraws/franzidraws1604/franzidraws160400050/55798423-seamless-banner-of-berlin-s-skyline-hand-drawn-black-and-white-illustration.jpg'
-);
+-- INSERT INTO main_app_city(name, image, country, grid_img) 
+-- VALUES (
+-- 'Barcelona',
+-- 'https://cdn.britannica.com/26/116526-050-76C37BBC/Cathedral-of-St-Basil-the-Blessed-Moscow.jpg',
+-- 'Spain',
+-- 'https://previews.123rf.com/images/franzidraws/franzidraws1604/franzidraws160400050/55798423-seamless-banner-of-berlin-s-skyline-hand-drawn-black-and-white-illustration.jpg'
+-- );
+
+-- INSERT INTO main_app_city(name, image, country, grid_img) 
+-- VALUES (
+-- 'Berlin',
+-- 'https://www.worldatlas.com/r/w1200/upload/7b/92/51/shutterstock-1011630226.jpg',
+-- 'Germany',
+-- 'https://previews.123rf.com/images/franzidraws/franzidraws1604/franzidraws160400050/55798423-seamless-banner-of-berlin-s-skyline-hand-drawn-black-and-white-illustration.jpg'
+-- );
+
+-- INSERT INTO main_app_city(name, image, country, grid_img) 
+-- VALUES (
+-- 'Havana',
+-- 'https://media.gq.com/photos/5914dd2943572d096b80c8f3/4:3/w_1728,h_1296,c_limit/guide-to-old-havana-cuba.jpg',
+-- 'Cuba',
+-- 'https://previews.123rf.com/images/franzidraws/franzidraws1606/franzidraws160600007/60559448-la-havane-cuba-dessin%C3%A9s-%C3%A0-la-main-illustration-en-noir-et-blanc.jpg'
+-- );
